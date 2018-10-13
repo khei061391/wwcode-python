aec2e0cc8eddae89e93188d4276ac7d70706a9db
khei061391 <44111416+khei061391@users.noreply.github.com>
2018-10-13 18:05:25 +0800

    convert to int

--- sec_python_code.py
+++ sec_python_code.py
@@ -1,4 +1,4 @@
-total_bill = input('How much is your total bill?')
-payment = input('How much is your payment?')
+total_bill = int(input('How much is your total bill?'))
+payment = int(input('How much is your payment?'))
 total = total_bill - payment
 print('Hi! Your change is {}'.format(total))
